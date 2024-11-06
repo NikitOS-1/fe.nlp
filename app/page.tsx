81b918a2cd569f66deaa422cd6487c828763740f
nikita <NikitaPuzyrenko93@gmail.com>
2024-11-07 01:26:49 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <Image
           className={styles.logo}
           src="/next.svg"
-          alt="Next.js logo"
+          alt="Next.js logo1"
           width={180}
           height={38}
           priority
